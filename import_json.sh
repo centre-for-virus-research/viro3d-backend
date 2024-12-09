dbba6ca49d3a4c3316696bdc35a1676780449feb
kcaj21 <alex.kcaj@gmail.com>
2024-12-09 18:02:27 +0000

    added auth to mongodb and template yaml file

--- import_json.sh
+++ import_json.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/usr/bin bash
+
+echo "Importing JSON data..."
 mongoimport --db viro3d --collection proteinstructures --file /data/import/proteinstructures.json --jsonArray
 mongoimport --db viro3d --collection genome_coordinates --file /data/import/genome_coordinates.json --jsonArray
+echo "JSON data imported."
