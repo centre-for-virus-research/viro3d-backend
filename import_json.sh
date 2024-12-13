1c55f5add82465de22274ddba752ba2755ad4e17
kcaj21 <alex.kcaj@gmail.com>
2024-12-13 14:33:04 +0000

    added clusters json to import json bash script

--- import_json.sh
+++ import_json.sh
@@ -3,4 +3,5 @@
 echo "Importing JSON data..."
 mongoimport --db viro3d --collection proteinstructures --file /data/import/proteinstructures.json --jsonArray
 mongoimport --db viro3d --collection genome_coordinates --file /data/import/genome_coordinates.json --jsonArray
+mongoimport --db viro3d --collection clusters --file /data/import/clusters.json --jsonArray
 echo "JSON data imported."
